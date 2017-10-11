f679564eec67e9f14844bb171efa1a54a300b849
Edwin Martin <edwin@bitstorm.org>
2017-10-12 00:04:34 +0200

    Fix closure externs

--- closure-externs.js
+++ closure-externs.js
@@ -1,4 +1,32 @@
 var define;
+define.amd;
 var module;
+module.exports;
 var $;
 var miq;
+miq.addClass;
+miq.append;
+miq.attr;
+miq.before;
+miq.clone;
+miq.closest;
+miq.css;
+miq.eq;
+miq.find;
+miq.first,
+miq.hasClass;
+miq.html;
+miq.is;
+miq.off;
+miq.on;
+miq.parent;
+miq.prop;
+miq.remove;
+miq.removeAttr;
+miq.removeClass;
+miq.text;
+miq.val;
+miq.ajax;
+miq.ajaxCallback;
+miq.miq
+miq.fn;
